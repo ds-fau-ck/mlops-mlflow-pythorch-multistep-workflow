7cbc867fb22eb3262659990b92f4c8559017250d
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-03 08:50:38 +0200

    part of set up

--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 conda create --prefix ./env python=3.7 -y
 source activate ./env 
-conda install pytorch torchvision torchaudio cpuonly -c pytorch
+conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
 pip install -r requirements.txt
 conda env export > conda.yaml
