11917eb855a8d7e0576c00dcf217b8f250f15d16
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-06-03 08:49:51 +0200

    donload the data

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 import shutil
-from tqdm import tqdm
+import torch
+from torchvision import datasets, transforms
+import mlflow
+import mlflow.pytorch
 import logging
 from src.utils.common import read_yaml, create_directories
+#from src.utils.model_utils import save_binary
 import random
 
 
@@ -20,8 +24,37 @@ logging.basicConfig(
 def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
-    pass
+    logging.info("define training kwargs")
+    train_kwargs= {"batch_size": config["params"]["BATCH_SIZE"]}
+    test_kwargs={"batch_size": config["params"]["TEST_BATCH_SIZE"]}
+    logging.info("updating device configuration as per cuda availability")
+    device_config={"DEVICE": 'cuda' if torch.cuda.is_available() else 'cpu'}
+    config.update(device_config)
+
+    if config["DEVICE"]=="cuda":
+        cuda_kwargs={"num_workers": 1, "pin_memory": True, "shuffle": True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+    logging.info("transforms added.")
+    transform=transforms.Compose(
+        [transforms.ToTensor()]
+    )
+    logging.info("Donloading training and testing data")
+    train=datasets.MNIST(config["source_data_dirs"]["data"], train=True, download=True, transform=transform)
+    test=datasets.MNIST(config["source_data_dirs"]["data"], train=False, download=True, transform=transform)
+    logging.info("defining training and testing data data loader")
+    train_loader=torch.utils.data.DataLoader(train, **train_kwargs)
+    test_loader=torch.utils.data.DataLoader(test, **test_kwargs)
+
+    #artifacts=config["artifacts"]
+    #model_config_dir=os.path.join(artifacts["artifacts_dir"], artifacts["model_config_dir"])
+    #create_directories([model_config_dir])
+    #train_loader_bin_file=artifacts["train_loader_bin_file"]
+    #train_loader_bin_filapath=os.path.join(model_config_dir,train_loader_bin_file)
+    #test_loader_bin_file=artifacts["test_loader_bin"]
+    #test_loader_bin_file_path=os.path.join(model_config_dir, test_loader_bin_file)
+    #save_binary()
+
 
 
 if __name__ == '__main__':
